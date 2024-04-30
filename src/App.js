e9767632fe0fc8bb5b6857b12b98bb191b291bdd
Soumik Pattanayak <soumik992001@gmail.com>
2024-04-30 14:52:55 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react';
 
-function App() {
+function FullNameForm() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === 'firstName') {
+      setFirstName(value);
+    } else if (name === 'lastName') {
+      setLastName(value);
+    }
+  };
+  useEffect(() => {
+    setFullName(fullName);
+  },[fullName]);
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    if (firstName && lastName) { // Check if both fields are filled
+      setFullName(`Full Name: ${firstName} ${lastName}`);
+    } else {
+      setFullName("");
+      alert('Please enter both first and last name');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <form onSubmit={handleSubmit}>
+      <h1>Full Name Display</h1>
+      <label htmlFor="firstName">First Name:</label>
+      <input
+        type="text"
+        id="firstName"
+        name="firstName"
+        value={firstName}
+        onChange={handleInputChange}
+        // required // Mark input as required
+      />
+      <br />
+      <label htmlFor="lastName">Last Name:</label>
+      <input
+        type="text"
+        id="lastName"
+        name="lastName"
+        value={lastName}
+        onChange={handleInputChange}
+        //required // Mark input as required
+      />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+    <div>{fullName}</div>
     </div>
   );
 }
 
-export default App;
+export default FullNameForm;
